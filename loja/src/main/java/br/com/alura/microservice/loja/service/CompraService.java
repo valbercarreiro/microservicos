1b0a96ef3b250a2e06e47ea056d030c29aecb70c
Valber Carreiro <valbercarreiro@gmail.com>
2021-05-14 10:48:00 -0300

    include services

---
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -0,0 +1,45 @@
+package br.com.alura.microservice.loja.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import br.com.alura.microservice.loja.client.FornecedorClient;
+import br.com.alura.microservice.loja.dto.CompraDTO;
+import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
+import br.com.alura.microservice.loja.dto.InfoPedidoDTO;
+import br.com.alura.microservice.loja.model.Compra;
+
+@Service
+public class CompraService {
+	
+	@Autowired
+	private FornecedorClient fornecedorClient;
+
+	@HystrixCommand(fallbackMethod = "realizaCompraFallback")
+	public Compra realizaCompra(CompraDTO compra) {
+		
+		final String estado = compra.getEndereco().getEstado();
+		
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(estado);
+		
+		InfoPedidoDTO infoPedido = fornecedorClient.realizaPedido(compra.getItens());
+		
+		Compra compraSalva = new Compra();
+		compraSalva.setPedidoId(infoPedido.getId());
+		compraSalva.setTempoDePreparo(infoPedido.getTempoDePreparo());
+		compraSalva.setEnderecoDestino(info.getEndereco());
+		
+		System.out.println(info.getEndereco());
+		
+		return compraSalva;
+	}
+	
+	public Compra realizaCompraFallback(CompraDTO compra) {
+		Compra compraFallback = new Compra();
+		compraFallback.setEnderecoDestino(compra.getEndereco().toString());
+		return compraFallback;
+	}
+	
+}
