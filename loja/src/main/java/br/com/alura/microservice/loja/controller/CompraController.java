31fc5b8a30721a16f00a74d31cce3671b323ffe9
Valber Carreiro <valbercarreiro@gmail.com>
2021-05-20 11:32:49 -0300

    autenticacao e autorizacao

--- loja/src/main/java/br/com/alura/microservice/loja/controller/CompraController.java
+++ loja/src/main/java/br/com/alura/microservice/loja/controller/CompraController.java
@@ -1,6 +1,8 @@
 package br.com.alura.microservice.loja.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
